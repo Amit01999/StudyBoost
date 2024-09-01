627f863445f1928415b6e3620ba9fd0fb5cd36a6
Amit01999 <amitsarkar019999@gmail.com>
2024-09-01 23:46:59 +0600

    database code change

--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,28 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-//const DATABASE_URL = process.env.MONGODB_URL || "mongodb://0.0.0.0:27017/Study_notion_db";
- 
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-
-exports.connect = () => {
-    mongoose.connect( process.env.MONGODB_URL , {
-        useNewUrlParser: true,
-        useUnifiedTopology:true,
-    })
-    .then(() => console.log("DB Connected Successfully"))
-    .catch( (error) => {
-        console.log("DB Connection Failed");
-         console.log(error);
-         process.exit(1);
-    } )
-};                              
+// exports.connect = () => {
+//   mongoose
+//     .connect(process.env.MONGODB_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     })
+//     .then(() => console.log('DB Connected Successfully'))
+//     .catch(error => {
+//       console.log('DB Connection Failed');
+//       console.log(error);
+//       process.exit(1);
+//     });
+// };
+exports.connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
